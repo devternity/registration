a2ca875c11ed5d78b32422999fba9102d9bfd9ef
Eduards Sizovs <eduards@sizovs.net>
2018-08-30 17:00:01 +0300

    add a nice hard-coded discount pattern :)

--- src/DiscountCtrl.ts
+++ src/DiscountCtrl.ts
@@ -1,26 +1,32 @@
-import {notification} from './Notification'
+import { notification } from './Notification'
 
 export class DiscountCtrl {
 
 
-	code: string;
+    code: string;
 
     static $inject = ['$http', '$rootScope'];	
 
-	constructor(private $http, private $rootScope) {}
-
-	apply = () => {
-		var url = "https://devternity-22e74.firebaseio.com/coupons/" + this.code + ".json"
-	    this.$http.get(url).then(_ => {    
-	    	if (_.data) {
-	    		notification.show('✅ Congratulations! The entered discount code is valid.');
-				this.$rootScope.$emit("ValidDiscountApplied", _.data);	    		
-	    	} else {
-				this.$rootScope.$emit("InvalidDiscountApplied", {});	    		
-				notification.show('❌ The entered discount code is not valid.');
+    constructor(private $http, private $rootScope) {}
+	
+    apply = () => {
+	    var url = "https://devternity-22e74.firebaseio.com/coupons/" + this.code + ".json"
+	    this.$http.get(url).then(_ => {
+            var discount = _.data
+            if (this.code.endsWith("18")) {
+                discount = {
+                    code: this.code,
+                    mainDayAmount: "25",
+                    workshopAmount: "25"
+                }
+            }
+            if (discount) {
+                notification.show('✅ Congratulations! The entered discount code is valid.');
+                this.$rootScope.$emit("ValidDiscountApplied", discount);
+    		} else {
+                this.$rootScope.$emit("InvalidDiscountApplied", {});
+                notification.show('❌ The entered discount code is not valid.');
 	    	}
-	    	
-	    });	
+       });
 	}
-
 }
